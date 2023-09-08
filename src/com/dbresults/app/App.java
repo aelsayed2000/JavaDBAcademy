461c7d9d62c0875839ccd8c62cb36408df00cfdb
aelsayed <ahmad.elsayed@dbresults.com.au>
2023-09-08 14:34:06 +1000

    Transforming Strings

--- src/com/dbresults/app/App.java
+++ src/com/dbresults/app/App.java
@@ -2,12 +2,23 @@ package com.dbresults.app;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Loan Calculator".toUpperCase());
+        System.out.println();
         int LoanAmount = 1000;
         int NumYears = 5;
         double IntRate = 1.5;
 
+        System.out.println("Calculating loan using:");
+
+        System.out.println("Loan Amount:  " + LoanAmount);
+
+        System.out.println("Number of Years: " + NumYears);
+
+        System.out.println("Interest Rate: " + IntRate);
+
+
         double Calculation = LoanAmount * (1 + (IntRate / 100) * NumYears);
-        String AmountOwed = "You owe: " + Calculation;
+        System.out.println("You owe: " + Calculation);
 
 
     }
