bf7f61a1180ff36cf469313c5da68d6423052088
aelsayed <ahmad.elsayed@dbresults.com.au>
2023-09-08 11:35:10 +1000

    Primitive types / Class Types

--- src/com/dbresults/app/App.java
+++ src/com/dbresults/app/App.java
@@ -2,7 +2,15 @@ package com.dbresults.app;
 
 public class App {
     public static void main(String[] args) {
+        // primitive types
         int age = 22;
         age = 23;
+        double temp = 8.0;
+        boolean isCold = true;
+
+        // class types
+        String.name = "Ahmad";
+        Person me = new Person();
+
     }
 }
