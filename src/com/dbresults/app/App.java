35e2f5f428bfcdc56e8beb10d3ce27ba4e66c80a
aelsayed <ahmad.elsayed@dbresults.com.au>
2023-09-08 14:56:08 +1000

    Conditionals

--- src/com/dbresults/app/App.java
+++ src/com/dbresults/app/App.java
@@ -2,24 +2,21 @@ package com.dbresults.app;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("Loan Calculator".toUpperCase());
         System.out.println();
-        int LoanAmount = 1000;
+        int LoanAmount = 50;
         int NumYears = 5;
         double IntRate = 1.5;
-
-        System.out.println("Calculating loan using:");
-
-        System.out.println("Loan Amount:  " + LoanAmount);
-
-        System.out.println("Number of Years: " + NumYears);
-
-        System.out.println("Interest Rate: " + IntRate);
-
-
-        double Calculation = LoanAmount * (1 + (IntRate / 100) * NumYears);
-        System.out.println("You owe: " + Calculation);
-
-
+        if (LoanAmount > 0 && NumYears > 0 && IntRate > 0) {
+            System.out.println("Calculating loan using:");
+            System.out.println("Loan Amount:  " + LoanAmount);
+            System.out.println("Number of Years: " + NumYears);
+            System.out.println("Interest Rate: " + IntRate);
+            double Calculation = LoanAmount * (1 + (IntRate / 100) * NumYears);
+            System.out.println("You owe: " + Calculation);
+        } else {
+            System.out.println("Cannot Compute.");
+        }
     }
 }
