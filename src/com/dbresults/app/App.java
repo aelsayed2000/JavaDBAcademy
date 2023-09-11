15ae8e1f58db4c369ae3771534d86450eb17f7f0
aelsayed <ahmad.elsayed@dbresults.com.au>
2023-09-11 10:01:30 +1000

    Refactoring

--- src/com/dbresults/app/App.java
+++ src/com/dbresults/app/App.java
@@ -5,18 +5,35 @@ public class App {
         System.out.println();
         System.out.println("Loan Calculator".toUpperCase());
         System.out.println();
-        int LoanAmount = 50;
-        int NumYears = 5;
-        double IntRate = 1.5;
-        if (LoanAmount > 0 && NumYears > 0 && IntRate > 0) {
-            System.out.println("Calculating loan using:");
-            System.out.println("Loan Amount:  " + LoanAmount);
-            System.out.println("Number of Years: " + NumYears);
-            System.out.println("Interest Rate: " + IntRate);
-            double Calculation = LoanAmount * (1 + (IntRate / 100) * NumYears);
-            System.out.println("You owe: " + Calculation);
+
+        int loanAmount = 50;
+        int numYears = 5;
+        double intRate = 1.5;
+        int yearsRemaining = numYears;
+
+        while (yearsRemaining > 0) {
+            loanAmount += loanAmount;
+            yearsRemaining--;
+        }
+
+        if (loanAmount > 0 && numYears > 0 && intRate > 0) {
+            PrintAmounts(loanAmount, numYears, intRate);
+
+            CalculateLoan(loanAmount, intRate, numYears);
         } else {
             System.out.println("Cannot Compute.");
         }
+    }
+
+    private static void CalculateLoan(int loanAmount, double intRate, int numYears) {
+        double calculation = loanAmount * (1 + (intRate / 100) * numYears);
+        System.out.println("You owe: " + calculation);
+    }
+
+    private static void PrintAmounts(int loanAmount, int numYears, double intRate) {
+        System.out.println("Calculating loan using:");
+        System.out.println("Loan Amount:  " + loanAmount);
+        System.out.println("Number of Years: " + numYears);
+        System.out.println("Interest Rate: " + intRate);
     }
 }
